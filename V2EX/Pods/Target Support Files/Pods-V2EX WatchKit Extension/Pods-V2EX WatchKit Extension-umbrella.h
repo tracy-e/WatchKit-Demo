046804904ecff3e9eacb7302c674b2e95820ceec
Tracy Yih <tracy.cpp@gmail.com>
2015-05-19 21:07:28 +0800

    update

--- V2EX/Pods/Target Support Files/Pods-V2EX WatchKit Extension/Pods-V2EX WatchKit Extension-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_V2EX_WatchKit_ExtensionVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_V2EX_WatchKit_ExtensionVersionString[];
-
